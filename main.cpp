708525c20eaab2fd1f3d26d4640f944c6d08e7fe
Waffle <totally_waffle@protonmail.com>
2025-07-11 03:58:10 -0600

    Functionality Changes
    minor changes all around, reformated input
    current version only works with linux do to hardcoded bracket (TODO)
    need to rewrite mod creation prompt (broken atm)
    
    Next commit should contain basic file browsing (Very TODO)

--- main.cpp
+++ main.cpp
@@ -8,48 +8,59 @@
 using namespace std;
 using namespace std::filesystem;
 
+void GenMod(string name, string version, path modDir, path gameDir);
+
 int main() {
 	//really all main is gonna be used for is building the directory variables and storage
 	//interface script should be seperate
-	bool prompt; //this is ugly :(
+	char prompt = '0'; //this is ugly :(
 	path gameDir, modDir;
-	string name, version;
+	string name, version, gameDirS, modDirS;
 	pair <string,string> files, dirs;
 
 	//will be removed for graphical interface
 	//terminal gui until release (may be repurposed)
 	cout << "Please enter your game directory: ";
-	cin >> gameDir;
+	 getline(cin, gameDirS);
 	cout << endl <<"Please enter your mod directory: ";
-	cin >> modDir;
+	 getline(cin, modDirS);
 
-	while(!exists(gameDir)) {
+	modDir = modDirS;
+	gameDir = gameDirS;
+
+while(!exists(gameDir)) {
 		cout << endl <<"Game directory does not exist";
 		cout << "Please enter your game directory: ";
 		cin >> gameDir;
-	}
+		gameDir = gameDirS;
+	};
+
 	//sheesh
 	//trap the user in a loop till they smarten up or create a new mod directory
 	if(!exists(modDir)) {
-		while(prompt = false) {
+		while(prompt == '0') {
 			if(!exists(modDir)) {
-				cout << endl << "Mod directory does not exist";
+				cout << endl << "Mod directory does not exist" << endl;
 				cout << "would you like to create a new mod directory? (0 or 1): ";
-				cin >> prompt;
+				cin.get(prompt);
+
+			};
 			if(exists(modDir)) {
 				break;
-			}
-		}
+			};
+		};
 		if(prompt = true) {
 			cout << endl << "Please enter the mod name: ";
-			cin >> name;
-			cout << endl << "Please enter the mod name: ";
-			cin >> version;
+			getline(cin, name);
+			cout << endl << "Please enter the mod version: ";
+			getline(cin, version);
+
 			create_directory(modDir);
 			GenMod(name, version, modDir, gameDir);
-		}
-	}
-}
+		};
+	};
+	return 0;
+};
 
 //this can be optimized by a mile but it works
 //I would still probably use the built in paradox launcher mod tool to do this
@@ -59,8 +70,8 @@ void GenMod(string name, string version, path modDir, path gameDir){
 	string gameVer;
 	//this part I am not proud of, I am basically just grabbing the game version from the provided game directory
 	//if this file isnt included then we have a real problem tbh
-	string dotModPath = modDir.u8string() += "descriptor.mod";
-	ofstream dotMod(dotModPath)
+	string dotModPath = modDir.u8string() += "/descriptor.mod";
+	ofstream dotMod(dotModPath);
 	string gameVerPath = gameDir.u8string() += "eu4_branch.txt";
 	ifstream gameVerFile(gameVerPath);
 	//void function so if we can't open the directory just silently kill the function
@@ -68,15 +79,15 @@ void GenMod(string name, string version, path modDir, path gameDir){
 		gameVerFile >> gameVer;
 		gameVer.erase(0, 8);
 		gameVer.erase(6, 3);
-		gameVer += "*";
+		gameVer += ".*";
 		contents.insert({"version=", version});
 		contents.insert({"tags={", "\"Alternative History\" }"}); //im gonna have to update this later tbh
 		contents.insert({"name=", name});
 		contents.insert({"supported_version=", gameVer});
 		for (const auto& pair : contents) {
-			dotMod >> pair.first >> pair.second >> endl;
-		}
+			dotMod << pair.first << pair.second << endl;
+		};
 		dotMod.close();
 		gameVerFile.close();
-	}
-}
+	};
+};
