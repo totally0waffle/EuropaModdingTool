736932540a0beeef68ed39e84e497e4bf7cc733e
totally0waffle <59709815+totally0waffle@users.noreply.github.com>
2025-08-07 14:59:37 -0600

    Update loader.cpp
    Added skeleton and checklist for preload script, relies on completion of  builder for complete functionality (anything related to reading and writing a file will be handled by builder)

--- src/loader.cpp
+++ src/loader.cpp
@@ -13,7 +13,23 @@ Builder builder;
 Flags appFlags;
 
 void preload() {
-
+    /*
+    bro I know this will get coded while high, just follow the list man try and implement it how either of us normally would (its a puzzle ffs) ~sober Waffle
+    
+    here we can abuse our builder script to preload everything. its a void statement so just call it timeout and continue execution
+    lets run through this step by step:
+    1. check the status of the mod folder, under the scenario it doesnt exist throw an exception (we can either solve this by making a new folder or reverting back to mod generator
+    2. copy the structure from eu4 over to the mod folder, we will use a cleaner afterwards to remove the fluff (we just want everything to be ready to be made
+    this will include things like our common folder and its sub folders, history, localization and the such
+    empty directories are fine but we do not need to create empty files for clarity sake.
+    3. index directory pathing for easier usage and navigation later
+    4. index current files to their parent folders and treat it like a linked list to find full pathing
+    5. Possibly validate our .mod file and offer an update?
+    6. (optional) index all of our variables, scopes and tags for quicker reading and writing (this will all be stored within a .json for better organization and modification later 
+    but how I see it, all of these should be stored within a list because we dont want to rely on text file reading each time as that could long and annoying... we also shouldnt have to worry
+    about overflow for this case atleast, this also leaves us open with the possibility of implementing support for games like hoi or victoria (any other clausewitz game I hope) 
+    */
+    
 }
 
 void finder(path master) {
